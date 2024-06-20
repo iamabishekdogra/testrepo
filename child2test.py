29f2ced78beb16ebbeda1735384eafd96eac223c
iamabishekdogra <abhi.saphiya@gmail.com>
2024-06-20 09:22:52 +0530

    first change

--- child2test.py
+++ child2test.py
@@ -1,6 +1,8 @@
 ## creating secound testrepo file 
+
 num1 = int(input("First No : "))
 num2 = int(input("Secound No : "))
+num3 = int(input("Third No : "))
 
-sum = num1+num2
-print("The sum of two no is", sum)
+sum = num1+num2+num3
+print("The sum of two no is ", sum)
